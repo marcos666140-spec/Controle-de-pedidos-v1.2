8894f6b247aca266e00ff44a49de980661c42af2
marcos666140-spec <marcos666140@gmail.com>
2026-03-12 23:14:27 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,19 +1,17 @@
-const tabs = document.getElementById("tabs")
-const conteudo = document.getElementById("conteudo")
+const tabs=document.getElementById("tabs")
+const conteudo=document.getElementById("conteudo")
 
-let setores = JSON.parse(localStorage.getItem("setores")) || [
+let setores=JSON.parse(localStorage.getItem("setores"))||[
 "Bebidas",
 "Confeitaria",
 "Panificação"
 ]
 
-let dados = JSON.parse(localStorage.getItem("dados")) || {}
+let dados=JSON.parse(localStorage.getItem("dados"))||{}
 
 function salvar(){
-
 localStorage.setItem("setores",JSON.stringify(setores))
 localStorage.setItem("dados",JSON.stringify(dados))
-
 }
 
 function criarTabs(){
@@ -24,7 +22,7 @@ setores.forEach((nome,i)=>{
 
 let t=document.createElement("div")
 t.className="tab"
-t.innerHTML=nome
+t.innerText=nome
 
 t.onclick=()=>abrirSetor(i)
 
@@ -43,19 +41,17 @@ conteudo.innerHTML=""
 
 let setor=setores[i]
 
+if(!dados[setor]) dados[setor]=[]
+
 let div=document.createElement("div")
 div.className="setor"
 
-if(!dados[setor]) dados[setor]=[]
-
 dados[setor].forEach(produto=>{
-
 div.appendChild(criarItem(setor,produto))
-
 })
 
 let botao=document.createElement("button")
-botao.innerHTML="+ Produto"
+botao.innerText="+ Produto"
 
 botao.onclick=()=>{
 
@@ -63,12 +59,12 @@ let nome=prompt("Nome do produto")
 
 if(!nome) return
 
-let novo={produto:nome,qtd:""}
-
-dados[setor].push(novo)
+dados[setor].push({
+produto:nome,
+qtd:""
+})
 
 salvar()
-
 abrirSetor(i)
 
 }
@@ -85,20 +81,46 @@ let div=document.createElement("div")
 div.className="item"
 
 let nome=document.createElement("div")
+nome.className="nomeProduto"
 nome.innerText=item.produto
-nome.style.fontWeight="600"
 
 let qtd=document.createElement("input")
+qtd.type="number"
 qtd.placeholder="Qtd"
-qtd.value=item.qtd
+qtd.value=item.qtd||""
 
 qtd.oninput=()=>{
 item.qtd=qtd.value
 salvar()
 }
 
+let botoes=document.createElement("div")
+botoes.className="botoesQtd"
+
+;[1,2,3,4].forEach(n=>{
+
+let b=document.createElement("button")
+b.innerText="+"+n
+
+b.onclick=()=>{
+
+let atual=parseInt(qtd.value)||0
+let novo=atual+n
+
+qtd.value=novo
+item.qtd=novo
+
+salvar()
+
+}
+
+botoes.appendChild(b)
+
+})
+
 div.appendChild(nome)
 div.appendChild(qtd)
+div.appendChild(botoes)
 
 return div
 
@@ -135,9 +157,7 @@ if(ativos.length==0) return
 texto+=setor.toUpperCase()+"\n"
 
 ativos.forEach(p=>{
-
 texto+=`${p.qtd} x ${p.produto}\n`
-
 })
 
 texto+="\n"
@@ -145,6 +165,7 @@ texto+="\n"
 })
 
 document.getElementById("pedidoFinal").classList.remove("hidden")
+
 document.getElementById("textoPedido").value=texto
 
 }
