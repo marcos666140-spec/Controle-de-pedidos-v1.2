7117d7a74306d8655f930308aa0b07f7c4f4d649
marcos666140-spec <marcos666140@gmail.com>
2026-03-13 16:45:00 -0300

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,31 +1,24 @@
-const CACHE = "pedidos-app-v1"
+const CACHE_NAME = "pedidos-cache-v1";
+const urlsToCache = [
+  "/",
+  "/index.html",
+  "/style.css",
+  "/script.js",
+  "/manifest.json",
+  "/icon-192.png",
+  "/icon-512.png"
+];
 
-const arquivos = [
-"/",
-"/index.html",
-"/style.css",
-"/script.js",
-"/manifest.json"
-]
-
-self.addEventListener("install", e => {
-
-e.waitUntil(
-caches.open(CACHE).then(cache=>{
-return cache.addAll(arquivos)
-})
-)
-
-})
+self.addEventListener("install", e=>{
+  e.waitUntil(
+    caches.open(CACHE_NAME).then(cache=>cache.addAll(urlsToCache))
+  );
+});
 
 self.addEventListener("fetch", e=>{
-
-e.respondWith(
-
-caches.match(e.request).then(res=>{
-return res || fetch(e.request)
-})
-
-)
-
-})
+  e.respondWith(
+    caches.match(e.request).then(response=>{
+      return response || fetch(e.request);
+    })
+  );
+});
